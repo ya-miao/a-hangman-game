365e6f015cf35f4d70141d457a3d6960a08d2819
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-25 16:35:40 -0400

    Hangman feature added

--- src/components/GameVisual.js
+++ src/components/GameVisual.js
@@ -1,9 +1,73 @@
 import { Box } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+import Figure from './Figure';
+import WrongLetters from './WrongLetters';
+import Word from './Word';
+import Popup from './Popup';
+import Notification from './Notification';
+import { showNotification as show, checkWin } from '../helpers/helpers';
+
+const words = ['application', 'programming', 'interface', 'wizard'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
 const GameVisual = () => {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+
+  }, [correctLetters, wrongLetters, playable]);
+
+  const buttons = "abcdefghjklmnopqrstuvwxyz".split("")
+
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
   return (
-    <Box>Component for the hangman visual</Box>
-  )
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
+    </>
+  );
 }
 
 export default GameVisual;
