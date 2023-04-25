444764cb8443bce16e23de0f7c7a6c9a5eb674cb
tril-studio <127056177+tril-studio@users.noreply.github.com>
2023-04-25 10:05:52 -0400

    Created base for Lighthall hangman

---
+++ src/components/GameInput.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+const GameInput = () => {
+  return (
+    <Box>Component for player input</Box>
+  );
+}
+
+export default GameInput;
---
+++ src/components/GameVisual.js
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+
+const GameVisual = () => {
+  return (
+    <Box>Component for the hangman visual</Box>
+  )
+}
+
+export default GameVisual;
