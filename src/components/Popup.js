377587bac5eba0e9aa41776f7f80fa86fd275762
Anthony Sanchez <antsanchez198@gmail.com>
2023-04-26 17:10:44 -0400

    added score

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from 'react';
 import { checkWin } from '../helpers/helpers';
 
-const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable, playAgain}) => {
+const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable, playAgain, tries}) => {
   let finalMessage = '';
+  let score = ''
   let finalMessageRevealWord = '';
   let playable = true;
 
   if( checkWin(correctLetters, wrongLetters, selectedWord) === 'win' ) {
-    finalMessage = 'Congratulations! You won! 😃';
+    finalMessage = 'You won! 😃';
+    score = 'Final Score: ' + tries;
     playable = false;
   } else if( checkWin(correctLetters, wrongLetters, selectedWord) === 'lose' ) {
     finalMessage = 'Unfortunately you lost. 😕';
     finalMessageRevealWord = `...the word was: ${selectedWord}`;
+    score = 'Final Score: ' + tries;
     playable = false;
   }
 
@@ -24,6 +27,7 @@ const Popup = ({correctLetters, wrongLetters, selectedWord, setPlayable, playAga
       <div className="popup">
         <h2>{finalMessage}</h2>
         <h3>{finalMessageRevealWord}</h3>
+        <h3>{score}</h3>
         <button onClick={playAgain}>Play Again</button>
       </div>
     </div>
